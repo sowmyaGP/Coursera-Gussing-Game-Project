36217efba048e8f24c54bdd7060eecd6c19e8c24
sowmyaGP <61739432+sowmyaGP@users.noreply.github.com>
2020-03-31 12:09:20 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+guessed=0
+function guessFolders() {
+while [[ $guessed -eq 0 ]]
+do
+	echo "Please guess the number of files in the current folder - "
+	read guess
+	fileCount=$(ls -l | wc -l)
+	if ! [[ $guess =~ ^[0-9]+$ ]]
+	then
+		echo "Please enter a valid number"
+	elif [[ $guess -eq $fileCount ]]
+	then
+		echo "You got it right"
+		guessed=1
+	elif [[ $guess -gt $fileCount ]]
+	then 
+		echo "Guess was higher than the actual value. Please guess again"
+	else 
+		echo "Guess was lower than the actual value. Please guess again"
+	fi
+done
+}
+guessFolders
